4c9a0f54bc0843534bdcd71e89688c780f85396f
victorCalazans <victorcalazansramos@gmail.com>
2022-05-19 07:38:58 -0300

    feat: x

--- src/components/Doubts/index.tsx
+++ src/components/Doubts/index.tsx
@@ -1,10 +1,8 @@
 import { Doubts as Layout } from "./Layout";
 
 export const Doubts = (props: any) => {
-  const x = "x";
   const layoutProps = {
     ...props,
-    x,
   };
 
   return <Layout {...layoutProps} />;
