c7f7aba88c69cedec5f1a1da82231e2c54bde6ce
victorCalazans <victorcalazansramos@gmail.com>
2022-05-23 13:54:10 -0300

    feat: new features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
-import { Application } from "../components/Application";
-import { Clinic } from "../components/Clinic";
+import { Application } from "../components/application";
+import { Clinic } from "../components/clinic";
 import { Contraction } from "../components/Contraction";
 import { Doubts } from "../components/Doubts";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { HowDidHelpYou } from "../components/HowDidHelpYou";
 import { Information } from "../components/Information";
-import { Plan } from "../components/Plan";
-import { Vantage } from "../components/Vantage";
+import { Plan } from "../components/plan";
 import { Toppings } from "../components/Toppings";
+import { Vantage } from "../components/vantage";
 
 import styles from "./styles.module.scss";
 
