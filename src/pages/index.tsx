b29db09e40dada2ec686437fd59d4149e7a84625
victorCalazans <victorcalazansramos@gmail.com>
2022-05-27 17:22:51 -0300

    fix: ajusting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
-import { Application } from "../components/Application";
-import { Clinic } from "../components/Clinic";
+import { Application } from "../components/application";
+import { Clinic } from "../components/clinic";
 import { Contraction } from "../components/Contraction";
 import { Doubts } from "../components/Doubts";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { HowDidHelpYou } from "../components/HowDidHelpYou";
 import { Information } from "../components/Information";
-import { Plan } from "../components/Plan";
+import { Plan } from "../components/plan";
 import { Toppings } from "../components/Toppings";
-import { Vantage } from "../components/Vantage";
+import { Vantage } from "../components/vantage";
 
 import styles from "./styles.module.scss";
 
@@ -25,9 +25,14 @@ export default function Home() {
         <meta name="description" content="plamev" />
 
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;500;700&display=swap" rel="stylesheet" />
-
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <div className={styles.Body}>
